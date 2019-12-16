09bafbacae49ca0e7ae2a8b7026678acd0658e10
David J. Rosenbaum <djr7c4@gmail.com>
2019-12-15 16:19:25 -0800

    Fix bug for Python 2.x and <3.8

--- pydoc.el
+++ pydoc.el
@@ -481,7 +481,7 @@ Adapted from `help-make-xrefs'."
     (shell-command-to-string
      ;; Use either importlib_metadata (a backport of importlib.metadata) or
      ;; importlib.metadata itself if it is available.
-     (concat python-shell-interpreter " -c \"try: import importlib_metadata;implib_meta = importlib_metadata\nexcept: pass\ntry: import importlib.metadata;implib_meta = importlib.metadata\nexcept: implib_meta = None\nmods = sorted(map(lambda x: x.metadata['name'], implib_meta.distributions())); print('({})'.format(' '.join(['\"{}\"'.format(x) for x in mods])))  \"")))))
+     (concat python-shell-interpreter " -c \"implib_meta = None\ntry: import importlib_metadata;implib_meta = importlib_metadata\nexcept: pass\ntry: import importlib.metadata;implib_meta = importlib.metadata\nexcept: pass\nmods = sorted(map(lambda x: x.metadata['name'], implib_meta.distributions())); print('({})'.format(' '.join(['\"{}\"'.format(x) for x in mods])))  \"")))))
 
 
 (defun pydoc-pkg-modules ()
