ee0660227b14d061325bcee5f35f2f25d8b4c0f3
Aniruddh Agarwal <ani@agrwl.dev>
2022-05-30 14:54:37 -0400

    Replace occurrences of 'python -m pydoc' with PYDOC-COMMAND

--- pydoc.el
+++ pydoc.el
@@ -505,7 +505,7 @@ Adapted from `help-make-xrefs'."
   (apply
    'append
    (mapcar (lambda (x) (split-string x " " t " "))
-	   (cdr (split-string  (shell-command-to-string (concat pydoc-python-command " -m pydoc topics")) "\n" t " ")))))
+	   (cdr (split-string  (shell-command-to-string (concat pydoc-command " topics")) "\n" t " ")))))
 
 
 (defun pydoc-keywords ()
@@ -513,7 +513,7 @@ Adapted from `help-make-xrefs'."
   (apply
    'append
    (mapcar (lambda (x) (split-string x " " t " "))
-	   (cdr (split-string  (shell-command-to-string (concat pydoc-python-command " -m pydoc keywords")) "\n" t " ")))))
+	   (cdr (split-string  (shell-command-to-string (concat pydoc-command " keywords")) "\n" t " ")))))
 
 
 (defvar *pydoc-all-modules*
